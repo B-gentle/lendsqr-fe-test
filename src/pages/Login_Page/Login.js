b80317854cc803540b5b6743cc50e561031f5898
beetech <brighteyo16@gmail.com>
2023-04-07 23:14:49 +0100

    finished working on the login page

--- src/pages/Login_Page/Login.js
+++ src/pages/Login_Page/Login.js
@@ -3,12 +3,13 @@ import { Button, Form, Container, Row, Col } from 'react-bootstrap';
 import img from '../../assets/pablo-sign-in-1.png';
 import logo from '../../assets/logo.svg';
 import './login.scss';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   const [showPassword, setShowPassword] = useState(false)
 
   return (
-    <Container className='login-page'>
+    <Container fluid className='login-page'>
       <img src={logo} alt='logo' className='logo' />
       <Row>
         <Col className='img-col' sm={12} lg={6}>
@@ -22,20 +23,22 @@ const Login = () => {
           </div>
           <Form className='login-form'>
             <Form.Group className="mb-3" controlId="email">
-              <Form.Control type="email" placeholder="Email" />
+              <Form.Control type="email" placeholder="Email" required />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="password" style={{position: 'relative'}}>
               <Form.Label>Password</Form.Label>
               <span className='show-password' onClick={()=>{setShowPassword(!showPassword)}}>Show</span>
-              <Form.Control type={showPassword ? 'text' : 'password'} placeholder="Password" />
+              <Form.Control type={showPassword ? 'text' : 'password'} placeholder="Password" required/>
             </Form.Group>
-            <Form.Text className="text-muted">
-              Forgot PASSWORD?
+            <Form.Text className="forgot-password">
+              FORGOT PASSWORD?
             </Form.Text>
+            <Link to='/dashboard'>
             <Button className='login-button' type="submit">
               LOG IN
             </Button>
+            </Link>
           </Form>
         </Col>
       </Row>
